c325f4904b6e2d357c7edd2a49e1e2861160923e
Andre He <andre.he@outlook.com>
2023-08-26 06:43:41 +0000

    working script; needs cleanup

--- m3ae/finetune_bridgedata.py
+++ m3ae/finetune_bridgedata.py
@@ -16,6 +16,7 @@ from jaxrl_m.data.text_processing import text_processors
 
 from flax import linen as nn
 from flax.jax_utils import prefetch_to_device
+from flax.training import checkpoints
 from jaxrl_m.data.bridge_dataset import BridgeDataset, glob_to_path_list
 from ml_collections import config_flags
 from tqdm.auto import tqdm, trange
@@ -66,7 +67,7 @@ FLAGS_DEF = define_flags_with_default(
     text_all_token_loss=False,
     dataloader_n_workers=0,
     dataloader_shuffle=False,
-    log_freq=50,
+    log_freq=100,
     plot_freq=1000,
     save_model_freq=0,
     image_loss_weight=1.0,
@@ -79,6 +80,7 @@ FLAGS_DEF = define_flags_with_default(
     lr_warmup_steps=0,
     weight_decay=0.05,
     load_checkpoint="",
+    save_dir="gs://rail-tpus-andre-v4/log/m3ae",
     m3ae=MaskedMultimodalAutoencoder.get_default_config(
         dict(
             image_mask_ratio=0.0,
@@ -278,7 +280,7 @@ def main(argv):
     )
 
     # do not use goal relabeling
-    FLAGS.config.dataset_kwargs["goal_relabeling_strategy"] = "last_state_upweighted"
+    FLAGS.config.dataset_kwargs["goal_relabeling_strategy"] = "last_state"
     FLAGS.config.dataset_kwargs["goal_relabeling_kwargs"] = dict(reached_proportion=0.0)
 
     train_data = BridgeDataset(
@@ -289,6 +291,7 @@ def main(argv):
         action_proprio_metadata=FLAGS.bridgedata_config.action_proprio_metadata,
         sample_weights=FLAGS.bridgedata_config.sample_weights,
         text_processor=text_processor,
+        start_and_end_only=True,
         **FLAGS.config.dataset_kwargs,
     )
     val_data = BridgeDataset(
@@ -296,11 +299,13 @@ def main(argv):
         FLAGS.seed,
         batch_size=FLAGS.batch_size,
         action_proprio_metadata=FLAGS.bridgedata_config.action_proprio_metadata,
-        train=False,
+        train=True,
         text_processor=text_processor,
+        start_and_end_only=True,
         **FLAGS.config.dataset_kwargs,
     )
     train_data_iter = train_data.get_iterator()
+    val_data_iter = val_data.get_iterator()
 
     image_patch_dim = FLAGS.patch_size * FLAGS.patch_size * 3
     image_sequence_length = (256 // FLAGS.patch_size) ** 2
@@ -387,13 +392,12 @@ def main(argv):
         model, learning_rate, encode_image, decode_image
     )
 
-    def generate_batch():
-        iterator = train_data_iter
+    def generate_batch(iterator):
         while True:
             batch = next(iterator)
             b = {}
-            b["image"] = (batch["observations"]["image"].astype(np.float32) - imagenet_mean) / imagenet_std
-            b["target_image"] = (batch["goals"]["image"].astype(np.float32) - imagenet_mean) / imagenet_std
+            b["image"] = (batch["observations"]["image"].astype(np.float32)/255.0 - imagenet_mean) / imagenet_std
+            b["target_image"] = (batch["goals"]["image"].astype(np.float32)/255.0 - imagenet_mean) / imagenet_std
             b["text"] = batch["goals"]["language"]["input_ids"].astype(np.int32)
             b["text_padding_mask"] = 1.0 - batch["goals"]["language"][
                 "attention_mask"
@@ -425,7 +429,8 @@ def main(argv):
             jnp.array(0, jnp.int32), jax_devices
         )
 
-    data_iterator = prefetch_to_device(generate_batch(), 2, jax_devices)
+    data_iterator = prefetch_to_device(generate_batch(train_data_iter), 2, jax_devices)
+    val_data_iterator = prefetch_to_device(generate_batch(val_data_iter), 2, jax_devices)
     step_counter = trange(0, total_steps, ncols=0)
 
     for step, batch in zip(step_counter, data_iterator):
@@ -445,21 +450,27 @@ def main(argv):
             tqdm.write("\n" + pprint.pformat(log_metrics) + "\n")
 
         if FLAGS.plot_freq > 0 and step % FLAGS.plot_freq == 0:
-            log_image = create_log_images(
-                jax.device_get(patch_predict_fn(state, sharded_rng, batch)),
-                mean=imagenet_mean,
-                std=imagenet_std,
-            )
-            #text_ids = jax.device_get(batch["text"])
-            #text_ids = text_ids.reshape((-1, *text_ids.shape[2:]))
-            #text = text_processor.decode(text_ids)[: len(log_image)]
+            n = 5
+            train_batch = batch
+            val_batch = next(val_data_iterator)
+            log_obj = {}
 
-            #table = wandb.Table(
-            #    columns=["images", "text"],
-            #    data=list(zip([wandb.Image(img) for img in log_image], text)),
-            #)
+            for pref, batch in [('train', train_batch), ('val', val_batch)]:
+                log_image = create_log_images(
+                    jax.device_get(patch_predict_fn(state, sharded_rng, batch)),
+                    mean=imagenet_mean,
+                    std=imagenet_std,
+                    n=n,
+                )
+                text_ids = jax.device_get(batch["text"])
+                text_ids = text_ids.reshape((-1, *text_ids.shape[2:]))
+                text = text_processor.decode(text_ids)[:n]
+                log_obj[f"{pref}_image_prediction"] = wandb.Image(log_image)
+                if jax_process_index == 0:
+                    tqdm.write(f"\n{pref} text\n" + pprint.pformat(text) + "\n")
+
             if jax_process_index == 0:
-                logger.log({"image_prediction": wandb.Image(log_image)})
+                logger.log(log_obj)
 
         if FLAGS.save_model_freq > 0 and step % FLAGS.save_model_freq == 0:
             save_data = {
@@ -467,17 +478,12 @@ def main(argv):
                 "variant": variant,
                 "state": jax.device_get(flax.jax_utils.unreplicate(state)),
             }
-            # if jax_process_index == 0:
-               #  logger.save_pickle(save_data, "model.pkl")
+            #if jax_process_index == 0:
+                #checkpoints.save_checkpoint(
+                #    FLAGS.save_dir, state, step=step,
+                #)
+                #  logger.save_pickle(save_data, "model.pkl")
 
-    if FLAGS.save_model_freq > 0:
-        save_data = {
-            "step": step,
-            "variant": variant,
-            "state": jax.device_get(flax.jax_utils.unreplicate(state)),
-        }
-        # if jax_process_index == 0:
-           # logger.save_pickle(save_data, "model.pkl")
 
 
 if __name__ == "__main__":
