14abce2209a55b4f56a81f414811af8e720fc703
AndreHe02 <andre.he@outlook.com>
2023-08-26 00:48:57 -0700

    clean up

--- m3ae/finetune_bridgedata.py
+++ m3ae/finetune_bridgedata.py
@@ -1,4 +1,5 @@
 import dataclasses
+import os
 import pprint
 from functools import partial
 
@@ -11,13 +12,11 @@ import numpy as np
 import optax
 import torch
 import wandb
-import os
-from jaxrl_m.data.text_processing import text_processors
-
 from flax import linen as nn
 from flax.jax_utils import prefetch_to_device
 from flax.training import checkpoints
 from jaxrl_m.data.bridge_dataset import BridgeDataset, glob_to_path_list
+from jaxrl_m.data.text_processing import text_processors
 from ml_collections import config_flags
 from tqdm.auto import tqdm, trange
 
@@ -47,8 +46,8 @@ from .utils import (
     define_flags_with_default,
     get_user_flags,
     image_float2int,
-    load_pickle,
     load_checkpoint,
+    load_pickle,
     set_random_seed,
 )
 
@@ -56,9 +55,13 @@ imagenet_mean = (0.485, 0.456, 0.406)
 imagenet_std = (0.229, 0.224, 0.225)
 
 FLAGS_DEF = define_flags_with_default(
+    bridgedata_path="gs://rail-tpus-homer-v4/data_new",
+    save_dir="gs://rail-tpus-andre-v4/log/m3ae",
+    load_checkpoint="/nfs/nfs2/users/andrehe/m3ae_public/m3ae_large.pkl",
     seed=42,
-    total_steps=50000,
+    total_steps=200000,
     batch_size=2,
+    num_log_examples=5,
     accumulate_grad_steps=1,
     patch_size=16,
     discretized_image=False,
@@ -67,20 +70,18 @@ FLAGS_DEF = define_flags_with_default(
     text_all_token_loss=False,
     dataloader_n_workers=0,
     dataloader_shuffle=False,
-    log_freq=100,
+    log_freq=1000,
     plot_freq=1000,
-    save_model_freq=0,
+    save_model_freq=10000,
     image_loss_weight=1.0,
     text_loss_weight=0.1,
     unpaired_text_loss_weight=0.0,
     clip_gradient=1e9,
     lr_init_value=0.0,
     lr_end_value=0.0,
-    lr_peak_value=1.5e-4,
+    lr_peak_value=5e-4,
     lr_warmup_steps=0,
-    weight_decay=0.05,
-    load_checkpoint="",
-    save_dir="gs://rail-tpus-andre-v4/log/m3ae",
+    weight_decay=0.00,
     m3ae=MaskedMultimodalAutoencoder.get_default_config(
         dict(
             image_mask_ratio=0.0,
@@ -91,7 +92,6 @@ FLAGS_DEF = define_flags_with_default(
     unpaired_text_data=TextDataset.get_default_config(),
     logging=WandBLogger.get_default_config(),
     log_all_worker=True,
-    bridgedata_path="gs://rail-tpus-homer-v4/data_new",
 )
 FLAGS = absl.flags.FLAGS
 
@@ -139,23 +139,11 @@ def create_train_step(model, learning_rate, encode_image=None, decode_image=None
                 None if FLAGS.image_all_token_loss else image_mask,
             )
             image_accuracy = 0.0
-            
+
             text_loss = 0.0
             text_accuracy = 0.0
-            # text_loss, text_accuracy = cross_entropy_loss_and_accuracy(
-            #     text_output,
-            #     text,
-            #     mask_intersection(
-            #         all_mask(text) if FLAGS.text_all_token_loss else text_mask,
-            #         mask_not(text_padding_mask),
-            #     ),
-            # )
 
-            loss = (
-                FLAGS.image_loss_weight
-                * image_loss
-                # + FLAGS.text_loss_weight * text_loss
-            )
+            loss = FLAGS.image_loss_weight * image_loss
 
             average_text_length = jnp.mean(
                 jnp.sum(mask_not(text_padding_mask), axis=-1)
@@ -279,7 +267,7 @@ def main(argv):
         encode_with_model=False,
     )
 
-    # do not use goal relabeling
+    # goal is always final state
     FLAGS.config.dataset_kwargs["goal_relabeling_strategy"] = "last_state"
     FLAGS.config.dataset_kwargs["goal_relabeling_kwargs"] = dict(reached_proportion=0.0)
 
@@ -291,7 +279,7 @@ def main(argv):
         action_proprio_metadata=FLAGS.bridgedata_config.action_proprio_metadata,
         sample_weights=FLAGS.bridgedata_config.sample_weights,
         text_processor=text_processor,
-        start_and_end_only=True,
+        # start_and_end_only=True,
         **FLAGS.config.dataset_kwargs,
     )
     val_data = BridgeDataset(
@@ -301,7 +289,7 @@ def main(argv):
         action_proprio_metadata=FLAGS.bridgedata_config.action_proprio_metadata,
         train=True,
         text_processor=text_processor,
-        start_and_end_only=True,
+        # start_and_end_only=True,
         **FLAGS.config.dataset_kwargs,
     )
     train_data_iter = train_data.get_iterator()
@@ -345,48 +333,39 @@ def main(argv):
         )
 
     tokenizer_max_length = 64
-    if False: # FLAGS.load_checkpoint != "":
-        #checkpoint_data = load_pickle(FLAGS.load_checkpoint)
-        #state = flax.jax_utils.replicate(checkpoint_data["state"], jax_devices)
-        #start_step = checkpoint_data["step"]
-        #del tokenizer_params
-        pass
-    else:
-        image = jnp.zeros(
-            (2, image_sequence_length, image_patch_dim), dtype=jnp.float32
-        )
-        text = jnp.zeros((2, tokenizer_max_length), dtype=jnp.int32)
-        text_padding_mask = jnp.zeros((2, tokenizer_max_length))
-        rngs = next_rng(keys=model.rng_keys())
-        params = model.init(rngs, image, text, text_padding_mask, deterministic=False)
 
-        params = flax.core.unfreeze(params)
-        if FLAGS.load_checkpoint != "":
-            checkpoint_data = load_checkpoint(FLAGS.load_checkpoint)
-            checkpoint_params = checkpoint_data["state"].params["params"]
-            params['params'] = checkpoint_params
-        
+    image = jnp.zeros((2, image_sequence_length, image_patch_dim), dtype=jnp.float32)
+    text = jnp.zeros((2, tokenizer_max_length), dtype=jnp.int32)
+    text_padding_mask = jnp.zeros((2, tokenizer_max_length))
+    rngs = next_rng(keys=model.rng_keys())
+    params = model.init(rngs, image, text, text_padding_mask, deterministic=False)
 
-        state = flax.jax_utils.replicate(
-            M3AETrainState.create(
-                params=flax.core.frozen_dict.unfreeze(params),
-                tokenizer_params=tokenizer_params,
-                apply_fn=None,
-                tx=optax.chain(
-                    optax.clip_by_global_norm(FLAGS.clip_gradient),
-                    optax.adamw(
-                        learning_rate=learning_rate,
-                        weight_decay=FLAGS.weight_decay,
-                        b1=0.9,
-                        b2=0.95,
-                        mask=get_weight_decay_mask,
-                    ),
+    params = flax.core.unfreeze(params)
+    if FLAGS.load_checkpoint != "":
+        checkpoint_data = load_checkpoint(FLAGS.load_checkpoint)
+        checkpoint_params = checkpoint_data["state"].params["params"]
+        params["params"] = checkpoint_params
+
+    state = flax.jax_utils.replicate(
+        M3AETrainState.create(
+            params=flax.core.frozen_dict.unfreeze(params),
+            tokenizer_params=tokenizer_params,
+            apply_fn=None,
+            tx=optax.chain(
+                optax.clip_by_global_norm(FLAGS.clip_gradient),
+                optax.adamw(
+                    learning_rate=learning_rate,
+                    weight_decay=FLAGS.weight_decay,
+                    b1=0.9,
+                    b2=0.95,
+                    mask=get_weight_decay_mask,
                 ),
             ),
-            jax_devices,
-        )
-        start_step = 0
-        del params, tokenizer_params
+        ),
+        jax_devices,
+    )
+    start_step = 0
+    del params, tokenizer_params
 
     train_step_fn, patch_predict_fn = create_train_step(
         model, learning_rate, encode_image, decode_image
@@ -396,13 +375,18 @@ def main(argv):
         while True:
             batch = next(iterator)
             b = {}
-            b["image"] = (batch["observations"]["image"].astype(np.float32)/255.0 - imagenet_mean) / imagenet_std
-            b["target_image"] = (batch["goals"]["image"].astype(np.float32)/255.0 - imagenet_mean) / imagenet_std
+            b["image"] = (
+                batch["observations"]["image"].astype(np.float32) / 255.0
+                - imagenet_mean
+            ) / imagenet_std
+            b["target_image"] = (
+                batch["goals"]["image"].astype(np.float32) / 255.0 - imagenet_mean
+            ) / imagenet_std
             b["text"] = batch["goals"]["language"]["input_ids"].astype(np.int32)
             b["text_padding_mask"] = 1.0 - batch["goals"]["language"][
                 "attention_mask"
             ].astype(np.float32)
-            
+
             b = jax.tree_map(
                 lambda x: x.reshape((n_devices, -1, *x.shape[1:])),
                 b,
@@ -430,7 +414,9 @@ def main(argv):
         )
 
     data_iterator = prefetch_to_device(generate_batch(train_data_iter), 2, jax_devices)
-    val_data_iterator = prefetch_to_device(generate_batch(val_data_iter), 2, jax_devices)
+    val_data_iterator = prefetch_to_device(
+        generate_batch(val_data_iter), 2, jax_devices
+    )
     step_counter = trange(0, total_steps, ncols=0)
 
     for step, batch in zip(step_counter, data_iterator):
@@ -449,25 +435,33 @@ def main(argv):
             logger.log(log_metrics)
             tqdm.write("\n" + pprint.pformat(log_metrics) + "\n")
 
+        # validation and logging code
         if FLAGS.plot_freq > 0 and step % FLAGS.plot_freq == 0:
-            n = 5
             train_batch = batch
             val_batch = next(val_data_iterator)
             log_obj = {}
 
-            for pref, batch in [('train', train_batch), ('val', val_batch)]:
+            for pref, batch in [("train", train_batch), ("val", val_batch)]:
                 log_image = create_log_images(
                     jax.device_get(patch_predict_fn(state, sharded_rng, batch)),
                     mean=imagenet_mean,
                     std=imagenet_std,
-                    n=n,
+                    n=FLAGS.num_log_examples,
                 )
                 text_ids = jax.device_get(batch["text"])
                 text_ids = text_ids.reshape((-1, *text_ids.shape[2:]))
-                text = text_processor.decode(text_ids)[:n]
+                text = text_processor.decode(text_ids)[: FLAGS.num_log_examples]
                 log_obj[f"{pref}_image_prediction"] = wandb.Image(log_image)
                 if jax_process_index == 0:
                     tqdm.write(f"\n{pref} text\n" + pprint.pformat(text) + "\n")
+
+            (_, val_metrics, _, _, _) = train_step_fn(
+                state, sharded_rng, accumulated_grads, accumulated_steps, val_batch
+            )
+            val_log_metrics = {"step": step}
+            val_log_metrics.update(get_metrics(val_metrics, unreplicate=True))
+            logger.log(val_log_metrics)
+            tqdm.write("\n" + pprint.pformat(val_log_metrics) + "\n")
 
             if jax_process_index == 0:
                 logger.log(log_obj)
@@ -478,12 +472,13 @@ def main(argv):
                 "variant": variant,
                 "state": jax.device_get(flax.jax_utils.unreplicate(state)),
             }
-            #if jax_process_index == 0:
-                #checkpoints.save_checkpoint(
-                #    FLAGS.save_dir, state, step=step,
-                #)
-                #  logger.save_pickle(save_data, "model.pkl")
 
+            if jax_process_index == 0:
+                checkpoints.save_checkpoint(
+                    FLAGS.save_dir,
+                    state,
+                    step=step,
+                )
 
 
 if __name__ == "__main__":
